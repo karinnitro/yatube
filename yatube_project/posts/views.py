a7e9872f2892ab15f775c18ec81cca192c5ca302
karinnitro <lugovaakarina78@gmail.com>
2024-03-15 13:16:45 +0300

    Добавление и подключение шаблонов

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    return render(request, "posts/index.html")
 def group_posts(request, slug):
-    return HttpResponse(f'Пост {slug}')
+    if slug == "pomidor":
+        return HttpResponse(f'<h2>Пост {slug}</h2><img src="https://foodcity.ru/storage/products/October2018/tFVuCnmXrEh4rWqkH3Gx.jpg"></img>')
+    else:
+        return HttpResponse(f'<h2>Пост {slug}</h2><img src="https://i.pinimg.com/564x/d1/b9/07/d1b907a965a32a7a3003043d507255d1.jpg"</img>')
+    
