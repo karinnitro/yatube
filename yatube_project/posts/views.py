c32be95e928470ad9a5596cf0935ea80c388ffd1
karinnitro <lugovaakarina78@gmail.com>
2024-03-16 14:30:49 +0300

    Создание главной и групповой страниц

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -5,8 +5,4 @@ from django.shortcuts import render
 def index(request):
     return render(request, "posts/index.html")
 def group_posts(request, slug):
-    if slug == "pomidor":
-        return HttpResponse(f'<h2>Пост {slug}</h2><img src="https://foodcity.ru/storage/products/October2018/tFVuCnmXrEh4rWqkH3Gx.jpg"></img>')
-    else:
-        return HttpResponse(f'<h2>Пост {slug}</h2><img src="https://i.pinimg.com/564x/d1/b9/07/d1b907a965a32a7a3003043d507255d1.jpg"</img>')
-    
+    return render(request, "posts/group_list.html")
