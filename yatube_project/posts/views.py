f291f26991722718a07c85f2fe181952a1694401
karinnitro <lugovaakarina78@gmail.com>
2024-06-13 20:01:40 +0300

    Добавление пагинации

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
 
-# Create your views here.
+
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
-        'title': "Последние обновления на сайте",
+        'page_obj': page_obj,
+        'title' : 'Последние обновления на сайте',
     }
     return render(request, 'posts/index.html', context)
 
@@ -24,3 +27,4 @@ def group_posts(request, slug):
         'description': group.description,
     }
     return render(request, 'posts/group_list.html', context)
+
